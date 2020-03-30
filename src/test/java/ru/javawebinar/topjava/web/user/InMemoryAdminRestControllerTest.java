ff9f89dc2bf7cc99f12a12ae6700006fb1daf47a
Petrusiova <petrusiovaolga@gmail.com>
2020-03-30 21:02:14 +0300

    fixed SpringMain, InMemory*-s

--- src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerTest.java
@@ -23,7 +23,8 @@ public class InMemoryAdminRestControllerTest {
 
     @BeforeClass
     public static void beforeClass() {
-        appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        appCtx = new ClassPathXmlApplicationContext(
+                "spring/spring-app-inmemory-test.xml", "spring/spring-db.xml");
         log.info("\n{}\n", Arrays.toString(appCtx.getBeanDefinitionNames()));
         controller = appCtx.getBean(AdminRestController.class);
     }
